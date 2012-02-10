664bf54e91a8f65ad9a8daf694a0419da4f72f1b
Trevor Nowak <teeerevor@gmail.com>
2012-02-10 15:21:06 +1100

    initial commit

---
+++ app/controllers/songs.rb
@@ -0,0 +1,29 @@
+Hotmess100.controllers :songs do
+  # get :index, :map => "/foo/bar" do
+  #   session[:foo] = "bar"
+  #   render 'index'
+  # end
+
+  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get "/example" do
+  #   "Hello world!"
+  # end
+
+  get :index, :provides => [:html, :js] do
+    @songs = Song.all(:order => 'name')
+    case content_type
+    when :js then @songs.to_json
+    else
+      render 'songs/index'
+    end
+  end
+end
